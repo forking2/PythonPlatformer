9ff9d9bc0d81a0144993bddff73a68781eb1ce9e
BobrickCS22 <132747159+BobrickCS22@users.noreply.github.com>
2024-07-15 12:00:10 +0300

    Add files via upload

--- RubyDum.py
+++ RubyDum.py
@@ -1,46 +1,119 @@
+# # import sys
+# import pygame
+# pygame.init()
+#
+#
+# # screen = pygame.display.set_mode((640, 480))
+# # sprite = pygame.image.load("1.png")
+# #
+# # screen.blit(sprite, (20, 20))
+# # pygame.quit()
+#
+#
+#
+# class Object(pygame.sprite.Sprite):
+#     def __init__(self,x,y,file, rect, image):
+#         pygame.sprite.Sprite.__init__(self)
+#
+#
+#
+#         self.image=pygame.image.load(file).convert_alpha()
+#         self.rect=self.image.get_rect(center=(x,y))
+#
+# width=1000
+# height=500
+#
+#
+# sc = pygame.display.set_mode((width,height))
+# clock = pygame.time.Clock()
+#
+# class player(Object):
+#     def __init__(self, x=100, y=400, file='1.png'):
+#         pygame.sprite.Sprite.__init__(self)
+#         self.x = x
+#         self.y = y
+#         self.file = file
+#
+#         self.image = pygame.image.load(file).convert_alpha()
+#         self.rect = self.image.get_rect(center=(x, y))
+#
+#
+# running = True
+#
+# while running:
+#     sc.fill(pygame.Color('white'))
+#
+#
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             exit()
+#
+#     clock.tick(60)
+#
+#
+# # screen.blit(sprite,(20,20))
+# sc.blit( player.rect,player.image)
+# pygame.display.flip()
+# pygame.display.update()
+
+
+
+
+
 
 
 import pygame
 pygame.init()
 
-
-class Object(pygame.sprite.Sprite):
-    def __init__(self,x,y,file, rect, image):
-        pygame.sprite.Sprite.__init__(self)
-
-        self.image=pygame.image.load(file).convert_alpha()
-        self.rect=self.image.get_rect(center=(x,y))
-
-width=1000
-height=500
-
-
-sc = pygame.display.set_mode((width,height))
+width = 1000
+height = 500
+sc = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
-class player(Object):
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, file, rect, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(file).convert_alpha()
+        self.rect = self.image.get_rect(center=(x, y))
+
+class Player(Object):
     def __init__(self, x=100, y=400, file='1.png'):
         pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.file = file
-
         self.image = pygame.image.load(file).convert_alpha()
         self.rect = self.image.get_rect(center=(x, y))
 
-
 running = True
-
 while running:
     sc.fill(pygame.Color('white'))
-
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
-
+            running = False
+    sc.blit(Player.image, Player.rect)
+    pygame.display.flip()
     clock.tick(60)
 
-sc.blit(player.image, player.rect)
-pygame.display.flip()
-pygame.display.update()
+pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
